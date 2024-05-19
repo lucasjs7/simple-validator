2c1a1840ad6cfe7f0d7520ba76baed859d5e8955
lucasjs7 <lucas_html@outlook.com>
2024-05-19 11:28:18 -0300

    _Float test updated

--- tests/Types/_FloatTest.php
+++ tests/Types/_FloatTest.php
@@ -1,81 +1,61 @@
 <?php
 
-use Lucasjs7\SimpleValidator\Language\Language as Lng;
-use Lucasjs7\SimpleValidator\Language\eLanguage as eLng;
+use Tests\Base;
 use Lucasjs7\SimpleValidator\Type\_Float;
 
-Lng::set(eLng::PT);
+$listTrue = [
+	'_Float#1 - T' => [
+		'test' => _Float::new()->max(10.3), 'value' => 10.3, 'result' => true
+	],
+	'_Float#2 - T' => [
+		'test' => _Float::new()->min(10.3), 'value' => 10.3, 'result' => true
+	],
+	'_Float#3 - T' => [
+		'test' => _Float::new()->unsigned(), 'value' => 0, 'result' => true
+	],
+	'_Float#4 - T' => [
+		'test' => _Float::new()->max(10.3)->min(5.8), 'value' => 10.3, 'result' => true
+	],
+	'_Float#5 - T' => [
+		'test' => _Float::new()->max(10.3)->min(5.8), 'value' => 5.8, 'result' => true
+	],
+	'_Float#6 - T' => [
+		'test' => _Float::new()->max(10.3)->unsigned(), 'value' => 0, 'result' => true
+	],
+	'_Float#7 - T' => [
+		'test' => _Float::new()->max(10.3)->unsigned(), 'value' => 10.3, 'result' => true
+	],
+];
 
-# TRUE TESTS
-it('_Float#1 - T', function () {
-	expect(
-		value: _Float::new()->max(10.3)->validate(10.3, false)
-	)->toBe(true);
-});
-it('_Float#2 - T', function () {
-	expect(
-		value: _Float::new()->min(10.3)->validate(10.3, false)
-	)->toBe(true);
-});
-it('_Float#3 - T', function () {
-	expect(
-		value: _Float::new()->unsigned()->validate(0, false)
-	)->toBe(true);
-});
-it('_Float#4 - T', function () {
-	expect(
-		value: _Float::new()->max(10.3)->min(5.8)->validate(10.3, false)
-	)->toBe(true);
-});
-it('_Float#5 - T', function () {
-	expect(
-		value: _Float::new()->max(10.3)->min(5.8)->validate(5.8, false)
-	)->toBe(true);
-});
-it('_Float#6 - T', function () {
-	expect(
-		value: _Float::new()->max(10.3)->unsigned()->validate(0, false)
-	)->toBe(true);
-});
-it('_Float#7 - T', function () {
-	expect(
-		value: _Float::new()->max(10.3)->unsigned()->validate(10.3, false)
-	)->toBe(true);
-});
+$listFalse = [
+	'_Float#1 - F' => [
+		'test' => _Float::new()->max(10.3), 'value' => 10.4, 'result' => false
+	],
+	'_Float#2 - F' => [
+		'test' => _Float::new()->min(10.3), 'value' => 10.2, 'result' => false
+	],
+	'_Float#3 - F' => [
+		'test' => _Float::new()->unsigned(), 'value' => -0.1, 'result' => false
+	],
+	'_Float#4 - F' => [
+		'test' => _Float::new()->max(10.3)->min(5.8), 'value' => 10.4, 'result' => false
+	],
+	'_Float#5 - F' => [
+		'test' => _Float::new()->max(10.3)->min(5.8), 'value' => 5.7, 'result' => false
+	],
+	'_Float#6 - F' => [
+		'test' => _Float::new()->max(10.3)->unsigned(), 'value' => -0.1, 'result' => false
+	],
+	'_Float#7 - F' => [
+		'test' => _Float::new()->max(10.3)->unsigned(), 'value' => 10.4, 'result' => false
+	],
+];
 
-# FALSE TESTS
-it('_Float#1 - F', function () {
-	expect(
-		value: _Float::new()->max(10.3)->validate(10.4, false)
-	)->toBe(false);
-});
-it('_Float#2 - F', function () {
-	expect(
-		value: _Float::new()->min(10.3)->validate(10.2, false)
-	)->toBe(false);
-});
-it('_Float#3 - F', function () {
-	expect(
-		value: _Float::new()->unsigned()->validate(-0.1, false)
-	)->toBe(false);
-});
-it('_Float#4 - F', function () {
-	expect(
-		value: _Float::new()->max(10.3)->min(5.8)->validate(10.4, false)
-	)->toBe(false);
-});
-it('_Float#5 - F', function () {
-	expect(
-		value: _Float::new()->max(10.3)->min(5.8)->validate(5.7, false)
-	)->toBe(false);
-});
-it('_Float#6 - F', function () {
-	expect(
-		value: _Float::new()->max(10.3)->unsigned()->validate(-0.1, false)
-	)->toBe(false);
-});
-it('_Float#7 - F', function () {
-	expect(
-		value: _Float::new()->max(10.3)->unsigned()->validate(10.4, false)
-	)->toBe(false);
-});
+Base::testTypeList($listTrue);
+Base::testTypeList($listFalse);
+
+Base::testMap('_Float/Map#1', $listTrue, true);
+Base::testMap('_Float/Map#2', $listFalse, false);
+
+Base::testStruct('_Float/Struct#1', $listTrue, true);
+Base::testStruct('_Float/Struct#2', $listFalse, false);
