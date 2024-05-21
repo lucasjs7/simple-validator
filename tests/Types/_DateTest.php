ccc8fa164fdb8db3c753488bdafff2a9af1d65d9
lucasjs7 <lucas_html@outlook.com>
2024-05-20 22:20:23 -0300

    _Date tests updated

--- tests/Types/_DateTest.php
+++ tests/Types/_DateTest.php
@@ -4,29 +4,65 @@ use Tests\Base;
 use Lucasjs7\SimpleValidator\Type\_Date;
 
 $listTrue = [
-	'_Date#1' => [
-		'test' => _Date::new()->format('d/m/Y'), 'value' => '1/1/1', 'result' => true
+	'_Date#' . __LINE__ => [
+		'test' => _Date::new()->format('d/m/Y'), 'value' => '01/01/0001', 'result' => true
 	],
-	'_Date#2' => [
-		'test' => _Date::new()->format('F'), 'value' => 'January', 'result' => true
+	'_Date#' . __LINE__ => [
+		'test' => _Date::new()->format('y-m-d'), 'value' => '01-01-01', 'result' => true
 	],
-	'_Date#3' => [
-		'test' => _Date::new()->format('y-m-d'), 'value' => '1-1-1', 'result' => true
+	'_Date#' . __LINE__ => [
+		'test' => _Date::new()->format('H'), 'value' => '1', 'result' => true
+	],
+	'_Date#' . __LINE__ => [
+		'test' => _Date::new()->format('Y-m-d H:i:s'), 'value' => '2014-02-28 12:12:12', 'result' => true
+	],
+	'_Date#' . __LINE__ => [
+		'test' => _Date::new()->format('Y-m-d'), 'value' => '2015-06-26', 'result' => true
+	],
+	'_Date#' . __LINE__ => [
+		'test' => _Date::new()->format('d/m/Y'), 'value' => '28/02/2014', 'result' => true
+	],
+	'_Date#' . __LINE__ => [
+		'test' => _Date::new()->format('H:i'), 'value' => '14:50', 'result' => true
+	],
+	'_Date#' . __LINE__ => [
+		'test' => _Date::new()->format('H'), 'value' => '14', 'result' => true
 	],
 ];
 
 $listFalse = [
-	'_Date#4' => [
+	'_Date#' . __LINE__ => [
+		'test' => _Date::new()->format('H'), 'value' => 14, 'result' => false
+	],
+	'_Date#' . __LINE__ => [
+		'test' => _Date::new()->format('y-m-d'), 'value' => '1-1-1', 'result' => false
+	],
+	'_Date#' . __LINE__ => [
+		'test' => _Date::new()->format('d/m/Y'), 'value' => '1/1/1', 'result' => false
+	],
+	'_Date#' . __LINE__ => [
 		'test' => _Date::new()->format('d/m/Y'), 'value' => '1-1-1', 'result' => false
 	],
-	'_Date#6' => [
-		'test' => _Date::new()->format('F'), 'value' => 'te', 'result' => false
-	],
-	'_Date#7' => [
+	'_Date#' . __LINE__ => [
 		'test' => _Date::new()->format('y-m-d'), 'value' => '1/1/1', 'result' => false
 	],
-	'_Date#8' => [
+	'_Date#' . __LINE__ => [
 		'test' => _Date::new()->format('y-m-d'), 'value' => 'a1-a1-a1', 'result' => false
+	],
+	'_Date#' . __LINE__ => [
+		'test' => _Date::new()->format('H'), 'value' => '-1', 'result' => false
+	],
+	'_Date#' . __LINE__ => [
+		'test' => _Date::new()->format('Y-m-d H:i:s'), 'value' => '2014-02-30 12:12:12', 'result' => false
+	],
+	'_Date#' . __LINE__ => [
+		'test' => _Date::new()->format('Y-m-d'), 'value' => '2015/06/26', 'result' => false
+	],
+	'_Date#' . __LINE__ => [
+		'test' => _Date::new()->format('d/m/Y'), 'value' => '30/02/2014', 'result' => false
+	],
+	'_Date#' . __LINE__ => [
+		'test' => _Date::new()->format('H:i'), 'value' => '14:77', 'result' => false
 	],
 ];
 
