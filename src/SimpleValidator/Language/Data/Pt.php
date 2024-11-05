77fec6eab84d6c4f95dabbd852042ffd510b185e
Lucas <lucasjs.developer@gmail.com>
2024-11-04 21:11:54 -0300

    fix PT language

--- src/SimpleValidator/Language/Data/Pt.php
+++ src/SimpleValidator/Language/Data/Pt.php
@@ -57,7 +57,7 @@ class Pt extends Data {
                     ],
                     'callable' => [
                         'invalid' => 'O valor é inválido.',
-                        'empty' => 'Não foi atribuida uma função para _Callable.',
+                        'empty' => 'Não foi atribuída uma função para _Callable.',
                         'anonymous' => 'A função atribuída a _Callable deve ser anônima.',
                     ],
                     'file' => [
